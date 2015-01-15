93787479e03806eb932be3a79f163bbadc4ec5e6
peter-gerhard <peter.gerhard90@gmail.com>
2015-01-15 12:59:43 +0100

    attribute definitions

--- app/model/sphere/CardDraft.scala
+++ app/model/sphere/CardDraft.scala
@@ -8,28 +8,30 @@ import io.sphere.sdk.attributes.{AttributeAccess, EnumAttributeDefinitionBuilder
 import io.sphere.sdk.models.{Referenceable, PlainEnumValue, LocalizedStrings}
 import io.sphere.sdk.products.{ProductVariantDraft, ProductVariantDraftBuilder, ProductDraftBuilder, ProductDraft}
 import io.sphere.sdk.producttypes.{ProductType, ProductTypeDraft}
-import io.sphere.sdk.attributes.BooleanAttributeDefinitionBuilder
+import io.sphere.sdk.attributes._
 import scala.collection.JavaConversions._
-
-object CardType {
-    val demand = PlainEnumValue.of("Demand", "Demand")
-
-    val values = PlainEnumValue.of("Demand", "Demand") :: PlainEnumValue.of("Offer", "Offer") :: Nil
-    val attribute = AttributeAccess.ofPlainEnumValue().getterSetter("cardType");
-}
 
 class DemandProductTypeDraftSupplier extends Supplier[ProductTypeDraft] {
 
   override def get = ProductTypeDraft.of("demand", "desc", createAttributes())
 
-  def createAttributes(): java.util.List[AttributeDefinition] = createSampleAttribute :: Nil
+  def createAttributes(): java.util.List[AttributeDefinition] = userIdAttribute :: Nil
 
-  def createSampleAttribute = BooleanAttributeDefinitionBuilder
-    .of("sample_name", LocalizedStrings.of(Locale.ENGLISH, "bla"))
+  def userIdAttribute = TextAttributeDefinitionBuilder
+    .of("userId", LocalizedStrings.of(Locale.ENGLISH, "userId"), TextInputHint.SINGLE_LINE)
+    .isRequired(true)
+    .build()
+
+  def distanceAttribute = NumberAttributeDefinitionBuilder
+    .of("distance", LocalizedStrings.of(Locale.ENGLISH, "distance"))
+    .isRequired(true)
+    .build()
+
+  def priceMinAttribute = MoneyAttributeDefinitionBuilder
+    .of("priceMin", LocalizedStrings.of(Locale.ENGLISH, "priceMin"))
+    .isRequired(true)
     .build()
 }
-
-//class DemandProductDraftSupplier(productType: Referenceable[ProductType], name: String) extends Supplier[ProductDraft] {}
 
 class DemandProductDraftSupplier(productTypeRef: Referenceable[ProductType], name: String)
   extends Supplier[ProductDraft] {
@@ -37,14 +39,14 @@ class DemandProductDraftSupplier(productTypeRef: Referenceable[ProductType], nam
 
   override def get(): ProductDraft = {
     val masterVariant: ProductVariantDraft = ProductVariantDraftBuilder.of()
-      .plusAttribute(CardType.attribute.valueOf(CardType.demand))
+//      .plusAttribute(CardType.attribute.valueOf(CardType.demand))
+      .plusAttribute(AttributeAccess.ofString().getterSetter("userId").valueOf(???))
       .build()
     val slug: LocalizedStrings = LocalizedStrings.of(Locale.ENGLISH, new Slugify().slugify(name))
 
     ProductDraftBuilder.of(productType, LocalizedStrings.of(Locale.ENGLISH, name), slug, masterVariant).build()
   }
 }
-
 
 //class CardProductTypeDraftSupplier extends Supplier[ProductTypeDraft] {
 //
@@ -57,6 +59,13 @@ class DemandProductDraftSupplier(productTypeRef: Referenceable[ProductType], nam
 //
 //    EnumAttributeDefinitionBuilder.of("cardType", cardTypeAttributeLabel, CardType.values).required(true).build()
 //  }
+//}
+
+//object CardType {
+//    val demand = PlainEnumValue.of("Demand", "Demand")
+//
+//    val values = PlainEnumValue.of("Demand", "Demand") :: PlainEnumValue.of("Offer", "Offer") :: Nil
+//    val attribute = AttributeAccess.ofPlainEnumValue().getterSetter("cardType");
 //}
 
 //class OfferProductTypeDraftSupplier extends Supplier[ProductTypeDraft] {
