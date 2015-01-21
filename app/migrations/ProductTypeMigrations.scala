ee7975a2bd4cf041c2030548c7c9a31a9987a493
peter-gerhard <peter.gerhard90@gmail.com>
2015-01-21 23:36:58 +0100

    refactored ProductTypeFactory to object ProductTypeDrafts

--- app/migrations/ProductTypeMigrations.scala
+++ app/migrations/ProductTypeMigrations.scala
@@ -6,7 +6,7 @@ import io.sphere.sdk.producttypes.{ProductTypeDraft, ProductType}
 import io.sphere.sdk.producttypes.commands.{ProductTypeCreateCommand}
 import io.sphere.sdk.producttypes.queries.ProductTypeQuery
 import io.sphere.sdk.queries.PagedQueryResult
-import model.sphere.{ProductTypeFactory}
+import model.sphere.ProductTypeDrafts
 import play.api.Logger
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -20,12 +20,12 @@ class ProductTypeMigrations(sphereClient: SphereClient) extends Migration {
   }
 
   override def run(): Unit = {
-    createType("demand", ProductTypeFactory.demandType)
-    createType("offer", ProductTypeFactory.offerType)
+    createType(ProductTypeDrafts.demand)
+    createType(ProductTypeDrafts.offer)
   }
 
-  def createType(configKey: String, typeDraft: ProductTypeDraft) = {
-    val typeName = Configloader.getStringOpt(s"$configKey.typeName").get
+  def createType(typeDraft: ProductTypeDraft) = {
+    val typeName = typeDraft.getName
     val queryResult: Future[PagedQueryResult[ProductType]] = sphereClient.execute(ProductTypeQuery.of().byName(typeName))
     val option: Future[Option[ProductType]] = queryResult.map(res => res.head())
 
