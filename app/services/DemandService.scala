91f0aaa9319c57775b9ffcdf81e8f9f029832c24
Stefan <stefanmeiwald@yahoo.de>
2015-01-24 14:21:25 +0100

    Broken get demand

--- app/services/DemandService.scala
+++ app/services/DemandService.scala
@@ -1,6 +1,6 @@
 package services
 
-import java.util.Locale
+import java.util.{Optional, Locale}
 
 import com.github.slugify.Slugify
 import common.domain._
@@ -10,6 +10,7 @@ import io.sphere.sdk.attributes.{AttributeAccess, Attribute}
 import io.sphere.sdk.models.LocalizedStrings
 import io.sphere.sdk.products
 import io.sphere.sdk.products.commands.ProductCreateCommand
+import io.sphere.sdk.products.queries.ProductFetchById
 import io.sphere.sdk.products.{ProductVariantDraftBuilder, ProductDraftBuilder}
 import model.sphere.{ProductTypeDrafts, ProductTypes}
 import model.{Demand, DemandId}
@@ -18,7 +19,7 @@ import play.api.Logger
 import play.api.libs.json.{JsValue, Json}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import common.helper.OptionalToOptionConverter.optionalToOption
+import common.helper.OptionalToOptionConverter._
 
 class DemandService(elasticsearch: ElasticsearchClient, sphereClient: SphereClient) {
   val demandIndex = IndexName("demands")
@@ -34,7 +35,43 @@ class DemandService(elasticsearch: ElasticsearchClient, sphereClient: SphereClie
     elasticsearch.search(demandIndex, demandType, QueryBuilders.matchAllQuery()).map(result => result.getHits.getHits)
   }
 
-  def getDemandById(id: DemandId): Option[Demand] = ???
+  def getDemandById(id: DemandId): Future[Option[Demand]] = {
+    val fetchCommand = ProductFetchById.of(id.value)
+    val futureProductOption: Future[Optional[products.Product]] = sphereClient.execute(fetchCommand)
+
+    futureProductOption.map {
+      productOption: Optional[products.Product] => productOption.map {
+        case Some(product: products.Product) => Some(Demand(
+                DemandId(product.getId),
+                UserId(getAttribute(product, "userId").getValue(AttributeAccess.ofString().attributeMapper())),
+                getAttribute(product, "tags").getValue(AttributeAccess.ofString().attributeMapper()),
+                Location(
+                  Longitude(getAttribute(product, "longitude").getValue(AttributeAccess.ofDouble().attributeMapper())),
+                  Latitude(getAttribute(product, "latitude").getValue(AttributeAccess.ofDouble().attributeMapper()))
+                ),
+                Distance(getAttribute(product, "distance").getValue(AttributeAccess.ofDouble().attributeMapper()).intValue()),
+                Price(getAttribute(product, "priceMin").getValue(AttributeAccess.ofMoney().attributeMapper()).getNumber.doubleValue()),
+                Price(getAttribute(product, "priceMax").getValue(AttributeAccess.ofMoney().attributeMapper()).getNumber.doubleValue())
+              ))
+        case _ => Option.empty[Demand]
+      }
+    }
+//      .map(productOpt => productOpt.map {
+//      case Some(product: products.Product) => Demand(
+//        DemandId(product.getId),
+//        UserId(getAttribute(product, "userId").getValue(AttributeAccess.ofString().attributeMapper())),
+//        getAttribute(product, "tags").getValue(AttributeAccess.ofString().attributeMapper()),
+//        Location(
+//          Longitude(getAttribute(product, "longitude").getValue(AttributeAccess.ofDouble().attributeMapper())),
+//          Latitude(getAttribute(product, "latitude").getValue(AttributeAccess.ofDouble().attributeMapper()))
+//        ),
+//        Distance(getAttribute(product, "distance").getValue(AttributeAccess.ofDouble().attributeMapper()).intValue()),
+//        Price(getAttribute(product, "priceMin").getValue(AttributeAccess.ofMoney().attributeMapper()).getNumber.doubleValue()),
+//        Price(getAttribute(product, "priceMax").getValue(AttributeAccess.ofMoney().attributeMapper()).getNumber.doubleValue())
+//      )
+//      case None => None
+//    }).recover { case e: Exception => None }
+  }
 
   def addDemand(demandDraft: DemandDraft): Future[Option[Demand]] = {
     for {
