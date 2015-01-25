d5d634cf5e3c1a3eb82fe7584b8357ff42602534
Stefan <stefanmeiwald@yahoo.de>
2015-01-25 16:24:55 +0100

    DemandServiceSpec stub

---
+++ test/services/DemandServiceSpec.scala
@@ -0,0 +1,12 @@
+package services
+
+import org.specs2.mutable.Specification
+
+class DemandServiceSpec extends Specification {
+
+  "Demand service" should {
+    "stub must" in {
+      true mustEqual true
+    }
+  }
+}
