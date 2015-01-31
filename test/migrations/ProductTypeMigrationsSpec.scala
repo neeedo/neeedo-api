a6f184b0856b398ca32d0b4548dd071fb9a9acba
Stefan <stefanmeiwald@yahoo.de>
2015-01-31 22:48:55 +0100

    Resolves #12 Bugfix for ProductTypeMigrationsSpec

--- test/migrations/ProductTypeMigrationsSpec.scala
+++ test/migrations/ProductTypeMigrationsSpec.scala
@@ -20,22 +20,21 @@ import scala.concurrent.{Await, Future}
 class ProductTypeMigrationsSpec extends Specification with Mockito {
   "ProductTypeMigrations" should {
 
-//    "create offer and demand product types when they don't exist in Sphere.IO" in TestApplications.loggingOffApp() {
-//      val sphereClient = mock[SphereClient]
-//      val emptyList: java.util.List[ProductType] = new java.util.ArrayList[ProductType]()
-//      val emptyPagedResult = PagedQueryResult.of(0,0,emptyList)
-//      //sphereClient.execute(ProductTypeQuery.of().byName(ProductTypeDrafts.demand.getName)) returns Future.successful(emptyPagedResult)
-//      //sphereClient.execute(ProductTypeQuery.of().byName(ProductTypeDrafts.offer.getName)) returns Future.successful(emptyPagedResult)
-//      sphereClient.execute(any[ProductTypeCreateCommand]) returns Future.successful(ProductTypeBuilder.of("","","", List.empty[AttributeDefinition].asJava).build())
-//
-//      val productTypeMigrations: ProductTypeMigrations = new ProductTypeMigrations(sphereClient)
-//      Await.result(productTypeMigrations.run(), new FiniteDuration(10, TimeUnit.SECONDS))
-//
-//      //there was one (sphereClient).execute(ProductTypeQuery.of().byName(ProductTypeDrafts.demand.getName))
-//      //there was one (sphereClient).execute(ProductTypeQuery.of().byName(ProductTypeDrafts.offer.getName))
-//      there was one (sphereClient).execute(ProductTypeCreateCommand.of(ProductTypeDrafts.demand))
-//      there was one (sphereClient).execute(ProductTypeCreateCommand.of(ProductTypeDrafts.offer))
-//    }
+    "create offer and demand product types when they don't exist in Sphere.IO" in TestApplications.loggingOffApp() {
+      val sphereClient = mock[SphereClient]
+      val emptyPagedResult = PagedQueryResult.empty[ProductType]()
+      sphereClient.execute(any[ProductTypeQuery]) returns Future.successful(emptyPagedResult)
+      sphereClient.execute(ProductTypeCreateCommand.of(ProductTypeDrafts.demand)) returns Future.successful(ProductTypeBuilder.of("","","", List.empty[AttributeDefinition].asJava).build())
+      sphereClient.execute(ProductTypeCreateCommand.of(ProductTypeDrafts.offer)) returns Future.successful(ProductTypeBuilder.of("","","", List.empty[AttributeDefinition].asJava).build())
+
+      val productTypeMigrations: ProductTypeMigrations = new ProductTypeMigrations(sphereClient)
+      Await.result(productTypeMigrations.run(), new FiniteDuration(10, TimeUnit.SECONDS))
+
+      there was one (sphereClient).execute(ProductTypeQuery.of().byName(ProductTypeDrafts.demand.getName))
+      there was one (sphereClient).execute(ProductTypeQuery.of().byName(ProductTypeDrafts.offer.getName))
+      there was one (sphereClient).execute(ProductTypeCreateCommand.of(ProductTypeDrafts.demand))
+      there was one (sphereClient).execute(ProductTypeCreateCommand.of(ProductTypeDrafts.offer))
+    }
 
     "do nothing when offer and demand producttypes exist in Sphere.IO" in TestApplications.loggingOffApp() {
       val sphereClient = mock[SphereClient]
