e2aaeae6bda5bd652482206dc3c8815904a5c9e2
Stefan <stefanmeiwald@yahoo.de>
2015-02-01 14:08:48 +0100

    Controllertest createDemand

--- test/controllers/DemandsSpec.scala
+++ test/controllers/DemandsSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 
 import common.domain._
+import model.{DemandId, Demand}
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import play.api.libs.json.Json
@@ -36,5 +37,37 @@ class DemandsSpec extends Specification with Mockito {
 
     Helpers.status(res) must equalTo(400)
     Helpers.contentAsString(res) must equalTo("{\"error\":\"Cannot parse json\"}")
+  }
+
+  "createDemands must return 400 unknown error when dermandService returns empty option" in {
+    val demandService = mock[DemandService]
+    val ctrl = new Demands(demandService)
+    val demandDraft = DemandDraft(UserId("1"), "socken bekleidung wolle", Location(Longitude(52.468562), Latitude(13.534212)), Distance(30), Price(25.0), Price(77.0))
+    demandService.createDemand(demandDraft) returns Future.successful(Option.empty)
+    val fakeHeaders = FakeHeaders(Seq(("Content-Type", Seq("application/json"))))
+    val fakeRequest = FakeRequest(Helpers.POST, "/")
+      .withHeaders(("Content-Type","application/json"))
+      .withJsonBody(Json.toJson(demandDraft))
+    val res: Future[Result] = ctrl.createDemand()(fakeRequest)
+
+    Helpers.status(res) must equalTo(400)
+    Helpers.contentAsString(res) must equalTo("{\"error\":\"Unknown error\"}")
+  }
+
+  "createDemands must return 200 when dermandService returns demand" in {
+    val demandService = mock[DemandService]
+    val ctrl = new Demands(demandService)
+    val demandDraft = DemandDraft(UserId("1"), "socken bekleidung wolle", Location(Longitude(52.468562), Latitude(13.534212)), Distance(30), Price(25.0), Price(77.0))
+    val demand = Demand(DemandId("1"), Version(1L), UserId("1"), "socken bekleidung wolle", Location(Longitude(52.468562), Latitude(13.534212)), Distance(30), Price(25.0), Price(77.0))
+    demandService.createDemand(demandDraft) returns Future.successful(Option(demand))
+
+    val fakeHeaders = FakeHeaders(Seq(("Content-Type", Seq("application/json"))))
+    val fakeRequest = FakeRequest(Helpers.POST, "/")
+      .withHeaders(("Content-Type","application/json"))
+      .withJsonBody(Json.toJson(demandDraft))
+    val res: Future[Result] = ctrl.createDemand()(fakeRequest)
+
+    Helpers.status(res) must equalTo(201)
+    Helpers.contentAsString(res) must equalTo(Json.obj("demand" -> Json.toJson(demand)).toString())
   }
 }
