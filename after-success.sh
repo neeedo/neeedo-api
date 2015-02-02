a8d7d839d533835f174ba2128a464aeddb9a08cd
Stefan <stefanmeiwald@yahoo.de>
2015-02-02 22:58:59 +0100

    Refactored buildscript. Only tested commits will be deployed now instead of master

--- after-success.sh
+++ after-success.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-codecov
-ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api; git pull origin master; ./sbt clean; ./sbt stage; ./neeedo restart; exit $?"
+#codecov
+function sshDeploy() {
+    printf -v __ %q "$1"
+    ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api; git pull origin master; git checkout $__;./sbt clean; ./sbt stage; ./neeedo restart; exit $?"
+}
+sshDeploy $TRAVIS_COMMIT
 exit $?
+
