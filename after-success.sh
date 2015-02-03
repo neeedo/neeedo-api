8a51588bc81cdf7c5e5cb3c414ed2410dc379a3f
Stefan Meiwald <MeiSign@users.noreply.github.com>
2015-02-03 15:31:31 +0100

    Update after-success.sh

--- after-success.sh
+++ after-success.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#codecov
-function sshDeploy() {
+codecov
+function sshDeploy {
     printf -v __ %q "$1"
     ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api; git pull origin master; git checkout $__;./sbt clean; ./sbt stage; ./neeedo restart; exit $?"
 }
