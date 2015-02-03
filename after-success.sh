1509e389e2b5874521a7f67469518a7d585f6c91
Stefan <stefanmeiwald@yahoo.de>
2015-02-03 18:37:37 +0100

    after-success script should be run with a bash instead of sh

--- after-success.sh
+++ after-success.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
-#codecov
-function sshDeploy() {
-    printf -v __ %q "$1"
-    ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api; git pull origin master; git checkout $__;./sbt clean; ./sbt stage; ./neeedo restart; exit $?"
-}
-sshDeploy $TRAVIS_COMMIT
-exit $?
-
+#!/bin/bash
+codecov
+printf -v __ %q "$TRAVIS_COMMIT"
+ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api;\
+git pull origin master;\
+git checkout $__;\
+./sbt clean;\
+./sbt stage;\
+./neeedo restart"
