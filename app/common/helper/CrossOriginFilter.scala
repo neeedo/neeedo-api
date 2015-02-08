e495d4c64925417001123c6f6d84100ceb48b53e
peter-gerhard <peter.gerhard90@gmail.com>
2015-02-08 16:33:49 +0100

    disabled false redirect routes

--- app/common/helper/CrossOriginFilter.scala
+++ app/common/helper/CrossOriginFilter.scala
@@ -8,7 +8,7 @@ object CrossOriginFilter extends Filter {
   def apply(nextFilter: (RequestHeader) => Future[Result])
            (requestHeader: RequestHeader): Future[Result] = {
     nextFilter(requestHeader).map { result =>
-      result.withHeaders("Access-Control-Allow-Origin" -> "*", 
+      result.withHeaders("Access-Control-Allow-Origin" -> "*",
                          "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS",
                          "Access-Control-Allow-Headers" -> "content-type, accept, origin",
                          "Access-Control-Max-Age" -> "86400")
--- app/controllers/Documentation.scala
+++ app/controllers/Documentation.scala
@@ -8,7 +8,7 @@ class Documentation extends Controller {
 //    // currently not working correct, do we need this anyways?
 //    Ok(service.readDocumentation())
     // TODO get readDocumentation() working again -> if we do a redirect here, cross origin request won't work!
-    Ok
-    // Redirect("https://github.com/HTW-Projekt-2014-Commercetools/api/blob/master/README.md")
+//    Ok
+    Redirect("https://github.com/HTW-Projekt-2014-Commercetools/api/blob/master/README.md")
   }
 }
