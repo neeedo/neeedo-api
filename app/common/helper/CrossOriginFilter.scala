f7c8174ce0360d10d46c578e1083a7e8cff230f2
Sascha Feldmann <sascha.feldmann@gmx.de>
2015-02-08 15:39:53 +0100

    #17 fixed and extended cross origin filters, removed redirect on OPTIONS because this will destroy cross-origin requests sent by browsers

--- app/common/helper/CrossOriginFilter.scala
+++ app/common/helper/CrossOriginFilter.scala
@@ -8,7 +8,10 @@ object CrossOriginFilter extends Filter {
   def apply(nextFilter: (RequestHeader) => Future[Result])
            (requestHeader: RequestHeader): Future[Result] = {
     nextFilter(requestHeader).map { result =>
-      result.withHeaders("ACCESS_CONTROL_ALLOW_ORIGIN" -> "*")
+      result.withHeaders("Access-Control-Allow-Origin" -> "*", 
+                         "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS",
+                         "Access-Control-Allow-Headers" -> "content-type, accept, origin",
+                         "Access-Control-Max-Age" -> "86400")
     }
   }
 }
--- app/controllers/Documentation.scala
+++ app/controllers/Documentation.scala
@@ -7,6 +7,8 @@ class Documentation extends Controller {
   def showDocumentation = Action {
 //    // currently not working correct, do we need this anyways?
 //    Ok(service.readDocumentation())
-    Redirect("https://github.com/HTW-Projekt-2014-Commercetools/api/blob/master/README.md")
+    // TODO get readDocumentation() working again -> if we do a redirect here, cross origin request won't work!
+    Ok
+    // Redirect("https://github.com/HTW-Projekt-2014-Commercetools/api/blob/master/README.md")
   }
 }
