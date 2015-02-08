b09075dc8398a996c0569f84076490ac57a54056
peter-gerhard <peter.gerhard90@gmail.com>
2015-02-08 20:16:39 +0100

    minimal cleanup

--- app/controllers/Documentation.scala
+++ app/controllers/Documentation.scala
@@ -5,10 +5,8 @@ import play.api.mvc._
 class Documentation extends Controller {
 
   def showDocumentation = Action {
-//    // currently not working correct, do we need this anyways?
+//    TODO get readDocumentation() working again
 //    Ok(service.readDocumentation())
-    // TODO get readDocumentation() working again -> if we do a redirect here, cross origin request won't work!
-//    Ok
     Redirect("https://github.com/HTW-Projekt-2014-Commercetools/api/blob/master/README.md")
   }
 }
