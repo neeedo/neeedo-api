249e1bcc0e82e7833a844e3acc5c4465ec298534
Stefan <stefanmeiwald@yahoo.de>
2015-02-25 22:11:54 +0100

    scp syntax

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp ../neeedo-api_1.0-SNAPSHOT_all.deb -oStrictHostKeyChecking=no deployuser@178.62.252.23:~
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ../neeedo-api_1.0-SNAPSHOT_all.deb deployuser@178.62.252.23:~
 #ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api;\
 #git pull origin master;\
 #git checkout $__;\
