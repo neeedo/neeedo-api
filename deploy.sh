fc9438757b73d70960a09fec44a8a6495cc1d45b
Stefan <stefanmeiwald@yahoo.de>
2015-02-25 21:53:50 +0100

    new keys added

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-printf -v __ %q "$TRAVIS_COMMIT"
-ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api;\
-git pull origin master;\
-git checkout $__;\
-./sbt clean;\
-./sbt stage;\
-./neeedo restart"
+scp ../neeedo-api_1.0-SNAPSHOT_all.deb -oStrictHostKeyChecking=no deployuser@178.62.252.23:~
+#ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd api;\
+#git pull origin master;\
+#git checkout $__;\
+#./sbt clean;\
+#./sbt stage;\
+#./neeedo restart"
