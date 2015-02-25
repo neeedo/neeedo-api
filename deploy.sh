3089eaadb28d5437f0c514d0983770854d996f67
Stefan <stefanmeiwald@yahoo.de>
2015-02-26 00:47:49 +0100

    Build app with sbt native packager. Resolves #7

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null target/neeedo-api_1.0-SNAPSHOT_all.deb deployuser@178.62.252.23:~
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null deployuser@178.62.252.23 "dpkg -i neeedo-api_1.0-SNAPSHOT_all.deb \
-/etc/init.d/neeedo-api start"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null deployuser@178.62.252.23 "sudo dpkg -i --force-confold neeedo-api_1.0-SNAPSHOT_all.deb \
+exit"
