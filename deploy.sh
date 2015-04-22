7395bac2cd6c3b53c42e9df9802aeb1965ff5d5b
Stefan Meiwald <stefanmeiwald@yahoo.de>
2015-04-22 23:40:58 +0200

    Added deployuser

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scp -i docker -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null target/neeedo-api_1.0-SNAPSHOT_all.deb root@46.101.162.213:~
-ssh -i docker -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@46.101.162.213 "sudo dpkg -i --force-confold neeedo-api_1.0-SNAPSHOT_all.deb \
+scp -i docker -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null target/neeedo-api_1.0-SNAPSHOT_all.deb neeedo-api@46.101.162.213:~
+ssh -i docker -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null neeedo-api@46.101.162.213 "sudo dpkg -i --force-confold neeedo-api_1.0-SNAPSHOT_all.deb \
 exit"
