e145b13dfc66fdc1f444ea7f2f954717453dd1d8
Peter Gerhard <peter.gerhard90@gmail.com>
2015-04-27 02:22:34 +0200

    added user deletion, fetch by mail, and rudimentary update

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import common.domain.{UserDraft, UserId}
+import common.domain.{Version, UserDraft, UserId}
 import common.helper.SecuredAction
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
@@ -10,12 +10,13 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class Users(userService: UserService) extends Controller {
-  def getUserByMail(mail: String) = SecuredAction.async {
-//    userService.getUserByMail(mail).map {
-//      case Some(user) => Ok(Json.toJson(user))
-//      case None => NotFound
-//    }
-    Future.successful(Ok)
+
+  // Todo enable secured actions, currently gives 301
+  def getUserByMail(mail: String) = Action.async {
+    userService.getUserByEmail(mail).map {
+      case Some(user) => Ok(Json.toJson(user))
+      case None => NotFound
+    }
   }
 
   def createUser = Action.async { implicit request =>
@@ -32,12 +33,24 @@ class Users(userService: UserService) extends Controller {
     }
   }
 
-  def updateUser(id: UserId, version: common.domain.Version) = Action.async {
-    Future.successful(Ok)
+  def updateUser(id: UserId, version: Version) = Action.async { implicit request =>
+    request.body.asJson match {
+      case Some(json) => json.asOpt[UserDraft] match {
+        case Some(draft) => userService.updateUser(id, version, draft).map {
+          case Some(user) => Ok(Json.obj("user" -> Json.toJson(user)))
+          case _ => BadRequest(Json.obj("error" -> "Unknown error"))
+        }
+        case None => Future.successful(BadRequest(Json.obj("error" -> "Cannot parse json")))
+      }
+      case None => Future.successful(BadRequest(Json.obj("error" -> "Missing body")))
+    }
   }
 
-  def deleteUser(id: UserId, version: common.domain.Version) = Action.async {
-    Future.successful(Ok)
+  def deleteUser(id: UserId, version: Version) = Action.async {
+    userService.deleteUser(id, version).map {
+      case Some(_) => Ok
+      case None => NotFound
+    }
   }
 
   //
--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -1,22 +1,33 @@
 package services
 
-import java.security.MessageDigest
-import java.util.Optional
-
 import common.domain._
 import common.helper.Wirehelper
+import common.helper.ImplicitConversions._
 import common.sphere.SphereClient
-import io.sphere.sdk.customers.{CustomerSignInResult, CustomerDraft, CustomerName}
-import io.sphere.sdk.customers.commands.{CustomerCreateCommand, CustomerSignInCommand}
-import scala.concurrent.Future
+import io.sphere.sdk.customers.queries.CustomerQuery
+import io.sphere.sdk.customers.{Customer, CustomerDraft, CustomerName}
+import io.sphere.sdk.customers.commands._
+import io.sphere.sdk.models.Versioned
+import java.security.MessageDigest
+import io.sphere.sdk.queries.{PagedQueryResult, QueryParameter}
 import play.api.cache.Cache
 import play.api.Play.current
+import scala.collection.JavaConverters._
+import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
 class UserService(sphereClient: SphereClient) {
 
-  def getUserByEmail(email: String) = Future.successful(None)
+  def getUserByEmail(email: String): Future[Option[User]] = {
+    val query = CustomerQuery.of().withAdditionalQueryParameters( List(QueryParameter.of("email", email)).asJava )
+
+    sphereClient.execute(query) map {
+      case (res: PagedQueryResult[Customer]) => res.head.asScala map {
+        case (customer: Customer) => User.fromCustomer(customer)
+      }
+    }
+  }
 
   def createUser(userDraft: UserDraft): Future[Option[User]] = {
     val customerName = CustomerName.ofFirstAndLastName(userDraft.username.value, "NonEmpty")
@@ -28,12 +39,16 @@ class UserService(sphereClient: SphereClient) {
   }
 
   def updateUser(id: UserId, version: Version, userDraft: UserDraft): Future[Option[User]] = {
-    // invalidate cache for user
-    Future.successful(None)
+    deleteUser(id, version)
+    createUser(userDraft)
   }
   def deleteUser(id: UserId, version: Version): Future[Option[User]] = {
-    // invalidate cache for user
-    Future.successful(None)
+    for {
+      customer <- sphereClient.execute(CustomerDeleteCommand.of(Versioned.of(id.value, version.value)))
+    } yield  {
+      Cache.remove(s"userCredentials.${customer.getEmail}")
+      Option(User.fromCustomer(customer))
+    }
   }
 
   def authorizeUser(credentials: UserCredentials): Future[Boolean] = {
