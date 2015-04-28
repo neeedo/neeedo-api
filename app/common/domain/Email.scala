9f04d2f90a0d4e4b7e9d8df4574de1818fe99ece
Peter Gerhard <peter.gerhard90@gmail.com>
2015-04-28 12:09:51 +0200

    added email url-decoding

---
+++ app/common/domain/Email.scala
@@ -0,0 +1,24 @@
+package common.domain
+
+import java.nio.charset.StandardCharsets
+
+import play.api.mvc.PathBindable
+
+case class Email(private val in: String) {
+  val value = play.utils.UriEncoding.decodePath(in, StandardCharsets.UTF_8.toString)
+}
+
+object Email {
+
+  implicit def pathBindable: PathBindable[Email] = new PathBindable[Email] {
+    override def bind(key: String, value: String): Either[String, Email] = {
+      Email(value) match {
+        case email: Email => Right(email)
+        case _	=> Left("Bla")
+      }
+    }
+
+    override def unbind(key: String, email: Email): String = email.value
+  }
+}
+
--- app/common/domain/UserCredentials.scala
+++ app/common/domain/UserCredentials.scala
@@ -1,21 +1,3 @@
 package common.domain
 
-import play.api.mvc.PathBindable
-
 case class UserCredentials(email: Email, password: String)
-
-case class Email(value: String) extends AnyVal
-
-object Email {
-
-  implicit def pathBindable: PathBindable[Email] = new PathBindable[Email] {
-    override def bind(key: String, value: String): Either[String, Email] = {
-      Email(value) match {
-        case email: Email => Right(email)
-        case _	=> Left("Bla")
-      }
-    }
-
-    override def unbind(key: String, email: Email): String = email.value
-  }
-}
