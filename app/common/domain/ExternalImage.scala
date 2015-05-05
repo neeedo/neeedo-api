e63bd3127ade89a45952126017a1951f3b6f70c1
Peter Gerhard <peter.gerhard90@gmail.com>
2015-05-05 18:48:19 +0200

    added ExternalImage reads

--- app/common/domain/ExternalImage.scala
+++ app/common/domain/ExternalImage.scala
@@ -1,9 +1,18 @@
 package common.domain
 
 import io.sphere.sdk.models.Image
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Reads}
 
 case class ExternalImage(url: Url, label: String)
 
 object ExternalImage {
+
+  // TODO Resolution Values
   def toSphereImage(img: ExternalImage) = Image.ofWidthAndHeight(img.url.value, 1280, 720, img.label)
+
+  implicit val externalImageReads: Reads[ExternalImage] = (
+    (JsPath \ "url").read[String] and
+    (JsPath \ "label").read[String]
+    ) { (url, label) => ExternalImage(Url(url), label) }
 }
