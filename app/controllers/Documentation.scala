f3b8352622e74c0d5ea214de873643a104d0f474
Peter Gerhard <peter.gerhard90@gmail.com>
2015-05-16 09:43:41 +0200

    fixed documentation

--- app/controllers/Documentation.scala
+++ app/controllers/Documentation.scala
@@ -5,8 +5,6 @@ import play.api.mvc._
 class Documentation extends Controller {
 
   def showDocumentation = Action {
-//    TODO get readDocumentation() working again
-//    Ok(service.readDocumentation())
-    Redirect("https://github.com/HTW-Projekt-2014-Commercetools/api/blob/master/README.md")
+    Redirect("https://github.com/neeedo/neeedo-api#readme")
   }
 }
