04c96beeefd09ae10d3ecd0974a1d114d89e9307
Peter Gerhard <peter.gerhard90@gmail.com>
2015-05-16 09:17:50 +0200

    removed delete action from images controller

--- app/controllers/Images.scala
+++ app/controllers/Images.scala
@@ -2,11 +2,9 @@ package controllers
 
 import common.helper.SecuredAction
 import common.helper.ImplicitConversions.ExceptionToResultConverter
-import model.OfferId
 import play.api.libs.json.{JsString, Json}
 import play.api.mvc.Controller
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import services.UploadService
 
 class Images(uploadService: UploadService) extends Controller {
@@ -17,10 +15,6 @@ class Images(uploadService: UploadService) extends Controller {
     } recover {
       case e: Exception => e.asResult
     }
-  }
-
-  def deleteImage(offerId: OfferId, imageHash: String) = SecuredAction.async {
-    Future(Ok)
   }
 
 }
