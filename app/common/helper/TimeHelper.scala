ef18e14a7dbf79f4285e67a95f6eabce3183ab4a
Stefan Meiwald <stefanmeiwald@yahoo.de>
2015-06-15 19:11:24 +0200

    Added Berlin Timezone

--- app/common/helper/TimeHelper.scala
+++ app/common/helper/TimeHelper.scala
@@ -1,7 +1,8 @@
 package common.helper
 
+import org.elasticsearch.common.joda.time.DateTimeZone
 import org.joda.time.DateTime
 
 class TimeHelper {
-  def now = new DateTime()
+  def now = new DateTime(DateTimeZone.forID("Europe/Berlin"))
 }
