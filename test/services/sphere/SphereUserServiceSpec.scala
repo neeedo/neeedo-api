fc9e00077c2bf238e4781fbc2d5cbccfc988a8b7
Peter Gerhard <peter.gerhard@commercetools.de>
2015-06-30 17:40:10 +0200

    Add spec for SphereUserService

--- test/services/sphere/SphereUserServiceSpec.scala
+++ test/services/sphere/SphereUserServiceSpec.scala
@@ -1,164 +1,132 @@
 package services.sphere
 
+
+import java.util.Optional
+
 import common.domain._
+import common.exceptions.{CustomerAlreadyExists, UserNotFound}
+import common.helper.UUIDHelper
 import common.sphere.SphereClient
-import io.sphere.sdk.customers.commands._
-import io.sphere.sdk.customers.{CustomerDraft, CustomerName, CustomerSignInResult}
+import io.sphere.sdk.client.ErrorResponseException
+import io.sphere.sdk.customers.commands.CustomerCreateCommand
+import io.sphere.sdk.customers.{CustomerSignInResult, CustomerDraft, CustomerName, Customer}
+import io.sphere.sdk.customers.queries.{CustomerByIdFetch, CustomerQuery}
+import io.sphere.sdk.models.{SphereError, ErrorResponse}
+import io.sphere.sdk.queries.PagedQueryResult
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import play.api.test.WithApplication
 
-import scala.concurrent.Future
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 class SphereUserServiceSpec extends Specification with Mockito {
 
   trait UserServiceContext extends WithApplication {
+    val sphereClient = mock[SphereClient]
+    val sphereUserService = new SphereUserService(sphereClient)
 
+    val uuid = new UUIDHelper
+
+    val userId = UserId(uuid.random)
+    val email = Email("test@test.com")
+    val username = Username("test")
+    val password = "test"
+
+    val userDraft = UserDraft(username, email, password)
+    val user = User(userId, Version(1L), username, email)
+    val userIdAndName = UserIdAndName(userId, username)
+
+    val pagedQueryResult = mock[PagedQueryResult[Customer]]
+    val customerSignInResult = mock[CustomerSignInResult]
+
+    val customer = mock[Customer]
+    customer.getId returns userId.value
+    customer.getVersion returns 1L
+    customer.getFirstName returns username.value
+    customer.getEmail returns email.value
+
+    val customerName = CustomerName.ofFirstAndLastName(username.value, "NonEmpty")
+    val customerDraft = CustomerDraft.of(customerName, email.value, password)
+
+    val customerByEmailQuery = CustomerQuery.of().byEmail(email.value)
+    val customerByIdFetch = CustomerByIdFetch.of(userId.value)
+    val customerCreateCommand = CustomerCreateCommand.of(customerDraft)
+
+    val duplicateFieldException = SphereError.of("DuplicateField", "")
+    val customerExistsException = mock[ErrorResponseException]
   }
 
-  "UserService" should {
-    "createUser successfully" in {
+  "SphereUserService.getUserByEmail" should {
 
-      val username: Username = Username("Blub")
-      val email: Email = Email("blub@blub.de")
-      val user = User(UserId("1"), Version(1L), username, email)
-      val pw: String = "pw123"
-      val userDraft = UserDraft(username, email, pw)
-      val customerName = CustomerName.ofFirstAndLastName(username.value, "NonEmpty")
-      val customerDraft = CustomerDraft.of(customerName, email.value, pw)
-      val customerCreateCommand = CustomerCreateCommand.of(customerDraft)
-      val sphere = mock[SphereClient]
-      val userService = new SphereUserService(sphere)
+    "return Option[User], " +
+      "if user with given email exists" in new UserServiceContext {
+      sphereClient.execute(any[CustomerQuery]) returns Future(pagedQueryResult)
+      pagedQueryResult.head() returns Optional.of(customer)
 
-      sphere.execute(customerCreateCommand) returns Future.successful(any[CustomerSignInResult])
+      Await.result(sphereUserService.getUserByEmail(email),
+        Duration(1, "second")) mustEqual Option(user)
 
-      userService.createUser(userDraft)
-      there was one (sphere).execute(customerCreateCommand)
+      there was one (sphereClient).execute(customerByEmailQuery)
+    }
+
+    "return Option.empty[User], " +
+      "if no user with given email exists" in new UserServiceContext {
+      sphereClient.execute(any[CustomerQuery]) returns Future(pagedQueryResult)
+      pagedQueryResult.head() returns Optional.empty()
+
+      Await.result(sphereUserService.getUserByEmail(email),
+        Duration(1, "second")) mustEqual Option.empty[User]
+
+      there was one (sphereClient).execute(customerByEmailQuery)
     }
   }
 
-//  "UserService.getUserByName" should {
-//    "return non empty Future[Option[User]]" in {
-//      this.getUserByNameSuccessfully
-//    }
-//
-//    "return empty Future[Option[User]]" in {
-//      this.failToGetUserByName
-//    }
-//  }
-//
-//  "UserService.createUser" should {
-//    "return non empty Future[Option[User]]" in {
-//      this.createUserSuccessfully
-//    }
-//
-//    "return empty Future[Option[User]]" in {
-//      this.failToCreateUser
-//    }
-//  }
-//
-//  "UserService.updateUser" should {
-//    "return non empty Future[Option[User]]" in {
-//      this.updateUserSuccessfully
-//    }
-//
-//    "return empty Future[Option[User]]" in {
-//      this.failToUpdateUser
-//    }
-//  }
-//
-//  "UserService.deleteUser" should {
-//    "return non empty Future[Option[User]]" in {
-//      this.deleteUserSuccessfully
-//    }
-//
-//    "return empty Future[Option[User]]" in {
-//      this.failToDeleteUser
-//    }
-//  }
-//
-//  "UserService.writeUserToSphere" should {
-//    "return non empty Future[Option[User]]" in {
-//      this.writeUserToSphereSuccessfully
-//    }
-//
-//    "return empty Future[Option[User]]" in {
-//      this.failToWriteUserToSphere
-//    }
-//  }
-//
-//  private def getUserByNameSuccessfully = {
-//    val userDraft = UserDraft(UserCredentials(Username("name"), "pw"))
-//    val userService = new UserService(mock[SphereClient])
-//    val user = userService.createUser(userDraft)
-//    userService.getUserByName(Username("name")) must beEqualTo(Option(user)).await
-//  }
-//
-//  private def failToGetUserByName = {
-//    val userService = new UserService(mock[SphereClient])
-//    userService.getUserByName(Username("not found")) must beEqualTo(Option.empty[User]).await
-//  }
-//
-//  private def createUserSuccessfully = {
-//    val user = User(UserId("id"), Version(1L), UserCredentials(Username("name"), "pw"))
-//    val userDraft = UserDraft(UserCredentials(Username("name"), "pw"))
-//    val sphere = mock[SphereClient]
-//    val userService = new UserService(sphere)
-//
-//    sphere.execute(any[CustomerCreateCommand]) returns Future.successful(Nothing)
-//    userService.createUser(userDraft) must beEqualTo(Option(user))
-//  }
-//
-//  private def failToCreateUser = {
-//    val userDraft = UserDraft(UserCredentials(Username("name"), "pw"))
-//    val sphere = mock[SphereClient]
-//    val userService = new UserService(sphere)
-//
-//    sphere.execute(any[CustomerCreateCommand]) returns Future.failed(new RuntimeException("test exception"))
-//    userService.createUser(userDraft) must beEqualTo(Option.empty[User])
-//  }
-//
-//  private def updateUserSuccessfully = {
-//    val user = User(UserId("id"), Version(1L), UserCredentials(Username("updatedName"), "updatedPw"))
-//    val updatedUserDraft = UserDraft(UserCredentials(Username("updatedName"), "updatedPw"))
-//    val sphere = mock[SphereClient]
-//    val userService = new UserService(mock[SphereClient])
-//
-//    sphere.execute(any[CustomerUpdateCommand]) returns Future.successful()
-//    userService.updateUser(user.id, user.version, updatedUserDraft) must beEqualTo(Option(user))
-//  }
-//
-//  private def failToUpdateUser = {
-//    val user = User(UserId("id"), Version(1L), UserCredentials(Username("updatedName"), "updatedPw"))
-//    val updatedUserDraft = UserDraft(UserCredentials(Username("updatedName"), "updatedPw"))
-//    val sphere = mock[SphereClient]
-//    val userService = new UserService(mock[SphereClient])
-//
-//    sphere.execute(any[CustomerUpdateCommand]) returns Future.failed(new RuntimeException("test exception"))
-//    userService.updateUser(user.id, user.version, updatedUserDraft) must beEqualTo(Option.empty[User])
-//  }
-//
-//  private def deleteUserSuccessfully = {
-//    val user = User(UserId("id"), Version(1L), UserCredentials(Username("updatedName"), "updatedPw"))
-//    val sphere = mock[SphereClient]
-//    val userService = new UserService(mock[SphereClient])
-//
-//    sphere.execute(any[CustomerDeleteCommand]) returns Future.failed(new RuntimeException("test exception"))
-//    userService.deleteUser(user.id, user.version) must beEqualTo(Option(user))
-//  }
-//
-//  private def failToDeleteUser = {
-//    val userService = new UserService(mock[SphereClient])
-//    userService.getUserByName(Username("Not Found"))
-//  }
-//
-//  private def writeUserToSphereSuccessfully = {
-//    val userService = new UserService(mock[SphereClient])
-//    userService.getUserByName(Username("Not Found"))
-//  }
-//
-//  private def failToWriteUserToSphere = {
-//    val userService = new UserService(mock[SphereClient])
-//    userService.getUserByName(Username("Not Found"))
-//  }
+  "SphereUserService.getUserById" should {
+
+    "return UserIdAndName, " +
+      "if user with given UserId exists" in new UserServiceContext {
+      sphereClient.execute(any[CustomerByIdFetch]) returns Future(Optional.of(customer))
+
+      Await.result(sphereUserService.getUserById(userId),
+        Duration(1, "second")) mustEqual userIdAndName
+
+      there was one (sphereClient).execute(customerByIdFetch)
+    }
+
+    "throw UserNotFound exception, " +
+      "if no user with given UserId exists" in new UserServiceContext {
+      sphereClient.execute(any[CustomerByIdFetch]) returns Future(Optional.empty())
+
+      Await.result(sphereUserService.getUserById(userId),
+        Duration(1, "second")) must throwA[UserNotFound]
+
+      there was one (sphereClient).execute(customerByIdFetch)
+    }
+  }
+
+  "SphereUserService.createUser" should {
+
+    "return User" in new UserServiceContext {
+      sphereClient.execute(customerCreateCommand) returns Future(customerSignInResult)
+      customerSignInResult.getCustomer returns customer
+
+      Await.result(sphereUserService.createUser(userDraft),
+        Duration(1, "second")) mustEqual user
+
+      there was one (sphereClient).execute(customerCreateCommand)
+    }
+
+    "throw CustomerAlreadyExists" in new UserServiceContext {
+      sphereClient.execute(customerCreateCommand) returns Future.failed(customerExistsException)
+      customerExistsException.getErrors returns List(duplicateFieldException).asJava
+
+      Await.result(sphereUserService.createUser(userDraft),
+        Duration(1, "second")) must throwA[CustomerAlreadyExists]
+
+      there was one (sphereClient).execute(customerCreateCommand)
+    }
+  }
+
 }
