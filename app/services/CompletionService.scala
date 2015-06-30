1d277b0582ff756ae8f37c47c1e83ae3f2294706
Peter Gerhard <peter.gerhard@commercetools.de>
2015-06-30 13:50:47 +0200

    Reformat code in CompletionService

--- app/services/CompletionService.scala
+++ app/services/CompletionService.scala
@@ -1,23 +1,20 @@
 package services
 
 import common.domain._
-import services.es.{EsSuggestionService, EsCompletionService}
+import services.es.{EsCompletionService, EsSuggestionService}
 
 import scala.concurrent.Future
 
 
 class CompletionService(esCompletionService: EsCompletionService, esSuggestionService: EsSuggestionService) {
 
-  def completeTag(tag: CompletionTag): Future[CompletionTagResult] = {
+  def completeTag(tag: CompletionTag): Future[CompletionTagResult] =
     esCompletionService.getCompletions(tag)
-  }
 
-  def suggestTags(phrase: CompletionPhrase): Future[CompletionPhraseResult] = {
-      esSuggestionService.getSuggestions(phrase)
-  }
+  def suggestTags(phrase: CompletionPhrase): Future[CompletionPhraseResult] =
+    esSuggestionService.getSuggestions(phrase)
 
-  def writeCompletions(tags: List[CompletionTag]) = {
+  def writeCompletions(tags: List[CompletionTag]) =
     esCompletionService.upsertCompletions(tags)
-  }
 }
 
