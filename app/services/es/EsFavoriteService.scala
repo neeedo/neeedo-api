bf2bb587eea163cdd7d6e6e1bb39ba6393da538e
Peter Gerhard <peter.gerhard@commercetools.de>
2015-07-07 22:44:20 +0200

    Refactor EsFavoriteService

--- app/services/es/EsFavoriteService.scala
+++ app/services/es/EsFavoriteService.scala
@@ -1,27 +1,23 @@
 package services.es
 
-import java.util.UUID
-
 import common.domain.{Favorite, UserId}
 import common.elasticsearch.ElasticsearchClient
 import common.exceptions.ElasticSearchIndexFailed
-import common.helper.ConfigLoader
 import common.helper.ImplicitConversions.ActionListenableFutureConverter
-import org.elasticsearch.index.query.QueryBuilders
+import common.helper.{ConfigLoader, UUIDHelper}
+import org.elasticsearch.index.query.{BoolQueryBuilder, QueryBuilders}
 import play.api.libs.json.Json
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class EsFavoriteService(elasticsearch: ElasticsearchClient, config: ConfigLoader) {
+class EsFavoriteService(elasticsearch: ElasticsearchClient, config: ConfigLoader, uuid: UUIDHelper) {
 
   val index = config.favoritesIndex
   val typeName = config.favoritesIndex.toTypeName
 
   def addFavorite(favorite: Favorite): Future[Favorite] = {
-    val uuid = UUID.randomUUID().toString
-
-    elasticsearch.indexDocument(uuid, index, typeName, Json.toJson(favorite)) map {
+    elasticsearch.indexDocument(uuid.random, index, typeName, Json.toJson(favorite)) map {
       result =>
         if(result.isCreated) favorite
         else throw new ElasticSearchIndexFailed("Error while saving favorite in elasticsearch")
@@ -40,13 +36,13 @@ class EsFavoriteService(elasticsearch: ElasticsearchClient, config: ConfigLoader
   def removeFavorite(favorite: Favorite): Future[Favorite] = {
     elasticsearch.client
       .prepareDeleteByQuery(index.value)
-      .setQuery(deleteFavoriteQuery(favorite))
+      .setQuery(buildDeleteFavoriteQuery(favorite))
       .execute()
       .asScala
       .map (_ => favorite)
   }
 
-  private[es] def deleteFavoriteQuery(favorite: Favorite) = QueryBuilders.boolQuery()
+  private[es] def buildDeleteFavoriteQuery(favorite: Favorite): BoolQueryBuilder = QueryBuilders.boolQuery()
     .must(QueryBuilders.termQuery("userId", favorite.userId.value))
     .must(QueryBuilders.termQuery("offerId", favorite.offerId.value))
 }
